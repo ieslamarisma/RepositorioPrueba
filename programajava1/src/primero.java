824cb49dae5893c233cdedda1327165ea26e4198
magb <magb@toshiba>
2013-01-08 21:37:35 +0100

    primer commint

---
+++ programajava1/src/primero.java
@@ -0,0 +1,22 @@
+// programa suma pares de 1 al 10
+// segundo
+
+
+public class primero {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int suma = 0;
+		for (int i = 2; i <= 10; i++) {
+			if (i % 2 == 0)
+				suma = suma + i;
+		
+		}
+		System.out.println("La suma de pares es ...  " + suma);
+		System.out.println(suma);
+
+	}
+}
