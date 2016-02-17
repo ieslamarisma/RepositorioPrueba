494e4d86c8dd0d6170002d489794e684e6b4b1aa
unikec <garcascris@gmail.com>
2016-02-17 10:59:22 +0100

    Update segundo.java

--- programajava2/src/segundo.java
+++ programajava2/src/segundo.java
@@ -7,6 +7,7 @@ public class segundo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("tercero");
+System.out.println("Hola a todos, cristina");
 	}
 
 }
